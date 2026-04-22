d820c5fc28e4d792600652eafc22374e1cd0eba2
Juanse1809 <juansebastiancc1809@gmail.com>
2026-04-21 22:09:22 -0500

    smooth wave divider transition with vertical gradient

--- src/components/WaveDivider.tsx
+++ src/components/WaveDivider.tsx
@@ -1,25 +1,21 @@
 export default function WaveDivider() {
   return (
-    <div className="relative -mt-1 bg-gradient-to-br from-blue-600 via-blue-500 to-blue-400 overflow-hidden">
+    <div className="relative -mt-1 overflow-hidden bg-gradient-to-b from-blue-500 to-white dark:to-slate-900">
       <svg
-        viewBox="0 0 1440 200"
+        viewBox="0 0 1440 160"
         preserveAspectRatio="none"
         xmlns="http://www.w3.org/2000/svg"
-        className="block w-full h-40 sm:h-52 lg:h-64"
+        className="block w-full h-32 sm:h-44 lg:h-52"
       >
         <path
-          d="M0,100 C240,200 480,0 720,100 C960,200 1200,0 1440,100 L1440,200 L0,200 Z"
-          fillOpacity="0.15"
-          className="fill-white dark:fill-slate-900"
+          d="M0,70 C360,20 720,110 1080,65 C1260,42 1380,88 1440,70 L1440,160 L0,160 Z"
+          fill="white"
+          fillOpacity="0.12"
         />
         <path
-          d="M0,120 C360,40 720,200 1080,120 C1260,75 1380,155 1440,120 L1440,200 L0,200 Z"
-          fillOpacity="0.45"
-          className="fill-white dark:fill-slate-900"
-        />
-        <path
-          d="M0,150 C280,95 560,200 840,155 C1100,110 1300,175 1440,150 L1440,200 L0,200 Z"
-          className="fill-white dark:fill-slate-900"
+          d="M0,95 C280,55 580,130 860,90 C1100,58 1310,115 1440,95 L1440,160 L0,160 Z"
+          fill="white"
+          fillOpacity="0.18"
         />
       </svg>
     </div>
